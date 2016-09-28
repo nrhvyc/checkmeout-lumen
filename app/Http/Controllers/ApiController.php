ae08d2d0e5d4b5c282fbc293d581acbb8532e4aa
Nick Hughes <nickryanhughes@gmail.com>
2016-09-27 19:51:59 -0500

    checkout endpoint only adds reservation if not checked out now

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -48,13 +48,19 @@ class ApiController extends Controller
                         // Checked Out
                         $reservation = Reservation::where('user_id', $user->id)
                                                   ->where('item_id', $item->id)
-                                                  ->orderBy('checkout_time', 'desc');
-                        dd($reservation);             
+                                                  ->orderBy('checkout_time', 'desc')
+                                                  ->first();
 
-                        Reservation::create(['user_id' => $user->id,
-                                             'item_id' => $item->id,
-                                             'checkout_time' => $datetime]);
-                        $items_saved++;
+                        // Insert Reservation If Item Not Checked Out Currently
+                        if(!$reservation && $reservation->checkin_time) {
+                            Reservation::create(['user_id' => $user->id,
+                                                 'item_id' => $item->id,
+                                                 'checkout_time' => $datetime]);
+                            $items_saved++;
+                        }
+                        else {
+                            $items_failed++;
+                        }
                     }
                     else {
                         $items_failed++;
