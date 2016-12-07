f0ea6138443000324d176419948c356ccf324f41
Jesse Hughes <Jesse Hughes>
2016-12-06 22:54:46 -0600

    Added filtering func to search

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -15,36 +15,56 @@ class ItemController extends Controller
     protected $validationRules = [];
 
     public function search(Request $request) {
-        /* jk not gonna use this right now
-        // Because of CheckGoogleOAuth Middleware id_token exist and is valid.
-        // This is hacky but will have to work for now.
-        $user = User::where('id_token', $request->input('id_token'));
-        */
-        /*
-        if (!$request->has('user_id')) {
-            return response('Bad request.', 400);
-        }
-
-        // Get the user
         $user_id = $request->input('user_id');
+
         $user = User::where('id', $user_id)->first();
-        */
+
         if (!$request->has('item_name')) {
             return response('Bad Request.', 400);
         }
 
         $item_name = $request->input('item_name');
 
-        $items = Item::where('name', 'LIKE', '%'. $item_name .'%')
-            ->orderBy('name', 'desc')->get();
+        if ($user) {
+            $stores = $user->stores;
+            $items = [];
 
-        $response = [
-          'code' => 200,
-          'status' => 'Successful',
-          'data' => ['items' => $items]
-        ];
+            $search_items = Item::where('name', 'LIKE', '%'. $item_name .'%')
+                         ->orderBy('name', 'desc')->get();
 
-        return response()->json($response);
+            foreach($stores as $store) {
+                $temp_items = $search_items->where('store_id', $store->id);
+                foreach($temp_items as $item) {
+                    $curr_item = Reservation::where('item_id', $item->id)
+                                            ->orderBy('checkout_time', 'desc')
+                                            ->first()
+                                            ->toArray();
+                    $arr_item = $item->toArray();
+                    if ($curr_item && $curr_item['checkin_time'] == NULL) {
+                        $arr_item['checked_out'] = 'true';
+                    } else {
+                        $arr_item['checked_out'] = 'false';
+                    }
+                    array_push($items, $arr_item);
+                }
+            }
+            $response = [
+              'code' => 200,
+              'status' => 'Successful',
+              'data' => ['items' => $items]
+            ];
+
+            return response()->json($response);
+        } else {
+          $response = [
+            'code' => 400,
+            'status' => 'Bad Request',
+            'data' => [],
+            'message' => 'user not found'
+          ];
+
+          return response()->json($response);
+        }
     }
 
     public function status(Request $request) {
@@ -79,6 +99,15 @@ class ItemController extends Controller
             ];
 
             return response()->json($response);
+        } else {
+          $response = [
+            'code' => 400,
+            'status' => 'Bad Request',
+            'data' => [],
+            'message' => 'user not found'
+          ];
+
+          return response()->json($response);
         }
     }
 }
